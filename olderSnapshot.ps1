d13916371a9d6f5d09f328a15d9e59d5b7cdfc6d
lance36 <lance36@gmail.com>
2017-12-05 11:33:02 +0100

    Update olderSnapshot.ps1

--- olderSnapshot.ps1
+++ olderSnapshot.ps1
@@ -9,4 +9,4 @@ $passwd = ConvertTo-SecureString $password -AsPlainText -Force
 $cred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $user, $passwd
 $nctlr = Connect-NcController $server -Credential $cred
 $olderthan = (get-date).adddays(-$days) 
-Get-Ncvol | Get-NcSnapshot  | where-object { $_.created -lt $olderthan } | Format-Table -Property Volumem,Name,Vserver,Created,Dependency -AutoSize
+Get-Ncvol | Get-NcSnapshot  | where-object { $_.created -lt $olderthan } | Format-Table -Property Vserver,Volume,Name,Created,Dependency -AutoSize
