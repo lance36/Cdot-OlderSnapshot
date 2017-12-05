7188efd317e345f6ca3b68652f70db21800f6076
lance36 <lance36@gmail.com>
2017-12-05 11:32:22 +0100

    Update olderSnapshot.ps1

--- olderSnapshot.ps1
+++ olderSnapshot.ps1
@@ -3,10 +3,10 @@ Param([parameter(Mandatory = $true)] [alias("s")] $server,
       [parameter(Mandatory = $true)] [alias("p")] $password,
       [parameter(Mandatory = $true)] [alias("d")] $days)
 
-#Import-Module C:\Windows\system32\WindowsPowerShell\v1.0\Modules\DataONTAP
+Import-Module DataONTAP
 
 $passwd = ConvertTo-SecureString $password -AsPlainText -Force
 $cred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $user, $passwd
 $nctlr = Connect-NcController $server -Credential $cred
-$olderthan = (get-date).adddays(-$days)
-Get-Ncvol | Get-NcSnapshot | where-object { $_.created -lt $olderthan }
+$olderthan = (get-date).adddays(-$days) 
+Get-Ncvol | Get-NcSnapshot  | where-object { $_.created -lt $olderthan } | Format-Table -Property Volumem,Name,Vserver,Created,Dependency -AutoSize
